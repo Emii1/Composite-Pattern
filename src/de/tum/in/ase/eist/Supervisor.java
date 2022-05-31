e516ab98981c739d9bc2b60b188440aca0e32180
Emi <emi.mano@tum.de>
2022-05-31 13:42:58 +0200

    abgabe

--- src/de/tum/in/ase/eist/Supervisor.java
+++ src/de/tum/in/ase/eist/Supervisor.java
@@ -2,7 +2,14 @@ package de.tum.in.ase.eist;
 
 public class Supervisor {
 
-	// TODO 2: Implement the Supervisor class
+    // TODO 2: Implement the Supervisor class
+    public void hireEmployee(Employee employee) {
 
-	// TODO 3: Implement listHierarchy() for Supervisor
+    }
+   public void fireEmployee(Employee employee){}
+
+    public void listHierarchy(int level){
+
+    }
+    // TODO 3: Implement listHierarchy() for Supervisor
 }
--- src/de/tum/in/ase/eist/Worker.java
+++ src/de/tum/in/ase/eist/Worker.java
@@ -1,7 +1,12 @@
 package de.tum.in.ase.eist;
 
 public class Worker {
-	// TODO 1: Implement the Worker class
+
+
+    // TODO 1: Implement the Worker class
+    public void listHierarchy(int level){
+
+    }
 
 	// TODO 3: Implement listHierarchy() for Worker
 }
