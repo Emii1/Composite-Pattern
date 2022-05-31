e5bcd7df531c4ee2d0dcb8a99251e11115b0fd42
Emi <emi.mano@tum.de>
2022-05-31 14:10:13 +0200

    abgabe

--- src/de/tum/in/ase/eist/Supervisor.java
+++ src/de/tum/in/ase/eist/Supervisor.java
@@ -3,22 +3,31 @@ package de.tum.in.ase.eist;
 import java.util.List;
 
 public abstract class Supervisor extends Employee {
-
     public Supervisor(String name) {
         super(name);
     }
 
+
     // TODO 2: Implement the Supervisor class
-    public void hireEmployee(Employee employee) {
+
+    public void hireEmployee(Employee employee)  {
 
     }
-   public void fireEmployee(Employee employee){}
+
+    public void fireEmployee(Employee employee) {
+
+   }
 
     public void listHierarchy(int level){
 
     }
 
+    public List<Employee> getSupervisedEmployees() {
+        return supervisedEmployees;
+    }
+
     private List<Employee> supervisedEmployees;
+
 
 
 
